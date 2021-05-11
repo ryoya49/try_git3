1ae93d80b040ecec5a1af8376b333cf48984d688
ryoya49 <ryoya@occ.co.jp>
2021-05-11 13:12:39 +0900

    edit main

--- try_git3/Program.cs
+++ try_git3/Program.cs
@@ -7,6 +7,7 @@ namespace try_git3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
